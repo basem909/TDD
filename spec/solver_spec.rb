300955a308d666b626e605720e33acf1dd5a7aad
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-05 19:01:55 -0300

    Add the 0 case for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,6 +7,11 @@ describe Solver do
       solver = solver.factorial(5)
       expect(solver).to equal 120
     end
+    it 'takes one argument, an integer N, and returns the factorial for that number' do
+      solver = Solver.new
+      solver = solver.factorial(0)
+      expect(solver).to equal 1
+    end
     it 'Returns an error if passed a negative value' do
       solver = Solver.new
       expect { solver.factorial(-3) }.to raise_error
