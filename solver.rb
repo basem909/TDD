205d12c1b3b0d309baac7e2af05bfa54ebb27988
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-05 17:40:39 -0300

    Code the factorial method and sucess the case

--- solver.rb
+++ solver.rb
@@ -1,5 +1,6 @@
 class Solver
   def factorial(int)
+  raise 'Please enter a positive no' if int < 0
   i = 1
   x = 1
   while i <= int
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,7 +4,10 @@ describe 'factorial' do
 it'takes one argument, an integer N, and returns the factorial for that number' do
 solver = Solver.new
 solver = solver.factorial(5)
-puts solver
 expect(solver).to equal 120
+end
+it'Returns an error if passed a negative value' do
+
+expect(Solver.new.factorial(-5)).to raise_error
 end
 end
