9242589282750ad7d8e497d5226e09d46812b62e
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-05 17:25:33 -0300

    Code the factorial method and sucess the case

--- solver.rb
+++ solver.rb
@@ -1,4 +1,11 @@
 class Solver
   def factorial(int)
+  i = 1
+  x = 1
+  while i <= int
+    x = x * i
+    i = i + 1
+  end
+  x
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../solver.rb'
 
-describe factorial do
+describe 'factorial' do
 it'takes one argument, an integer N, and returns the factorial for that number' do
 solver = Solver.new
 solver = solver.factorial(5)
-expect (solver).to eql 120
+puts solver
+expect(solver).to equal 120
 end
 end
