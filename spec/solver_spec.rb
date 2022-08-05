47a93975748f7024d0b475f4eebdfbad1730eb67
gedewon <gedewon.hayle.9@gmail.com>
2022-08-05 23:47:44 +0300

    Test case for solver

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,13 +1,24 @@
 require_relative '../solver.rb'
 
-describe 'factorial' do
+describe Solver do
+   context 'Factorial' do
     it'takes one argument, an integer N, and returns the factorial for that number' do
     solver = Solver.new
     solver = solver.factorial(5)
     expect(solver).to equal 120
+    end
+        it'Returns an error if passed a negative value' do
+        solver = Solver.new
+        expect{solver.factorial(-3)}.to raise_error
+    end
+    end
+    
+    context 'Reverse' do
+        it'takes one argument, an integer N, and returns the factorial for that number' do
+        solver = Solver.new
+        solver = solver.reverse('hello')
+        expect(solver).to equal 'olleh'
+        end
+    end
 end
-    it'Returns an error if passed a negative value' do
-    solver = Solver.new
-    expect{solver.factorial(-3)}.to raise_error
-end
-end
+
