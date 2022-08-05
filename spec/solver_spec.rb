f88d3995200c02af1f1e69493db90e2e21810e95
gedewon <gedewon.hayle.9@gmail.com>
2022-08-05 23:56:23 +0300

    Test case for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,4 +20,28 @@ describe Solver do
       expect(solver).to eq 'olleh'
     end
   end
+
+  context 'Fizzbuzz' do
+    it 'should When N is divisible by 3, return "fizz".' do
+      solver = Solver.new
+      solver = solver.fizzbuzz(6)
+      expect(solver).to eq 'fizz'
+    end
+    it 'When N is divisible by 5, return "buzz".' do
+        solver = Solver.new
+        solver = solver.fizzbuzz(10)
+        expect(solver).to eq 'buzz'
+    end
+    it 'should When N is divisible by 3 and 5, return "fizzbuzz"' do
+        solver = Solver.new
+        solver = solver.fizzbuzz(15)
+        expect(solver).to eq 'fizzbuzz'
+    end
+    it 'Any other case, return N as a string (e.g. say N is 7 then return "7").' do
+        solver = Solver.new
+        solver = solver.fizzbuzz(7)
+        expect(solver).to eq '7'
+    end
+  end
+
 end
