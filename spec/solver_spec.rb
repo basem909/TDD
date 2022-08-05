6f98a49cb5ce050d37671d9c7220deb1eeb3a136
gedewon <gedewon.hayle.9@gmail.com>
2022-08-05 23:43:58 +0300

    Raise handler

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../solver.rb'
 
 describe 'factorial' do
-it'takes one argument, an integer N, and returns the factorial for that number' do
-solver = Solver.new
-solver = solver.factorial(5)
-expect(solver).to equal 120
+    it'takes one argument, an integer N, and returns the factorial for that number' do
+    solver = Solver.new
+    solver = solver.factorial(5)
+    expect(solver).to equal 120
 end
-it'Returns an error if passed a negative value' do
-
-expect(Solver.new.factorial(-5)).to raise_error
+    it'Returns an error if passed a negative value' do
+    solver = Solver.new
+    expect{solver.factorial(-3)}.to raise_error
 end
 end
